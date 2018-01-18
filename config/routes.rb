9c71ddcb11e98425b938982e50571675d5c59806
Andrew J Wippler <andrew.wippler@gmail.com>
2018-01-18 10:53:31 -0600

    Fixes to places

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get 'tags/search' => 'tags#search'
   resources :tags
   resources :illustrations
-  resources :places  
+  resources :places
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
